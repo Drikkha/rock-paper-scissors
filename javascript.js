aef6a93e738ceda6c85da4ee07fea221bf8372ad
EspindolaCristian <Drikkha275@gmail.com>
2023-02-20 12:54:36 -0300

    interface added

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+let compCounter = 0;
+let playerCounter = 0;
+let gamesCounter = 0;
+
 function random(number = 1){
     return(Math.ceil(Math.random() * number));
 }
@@ -13,7 +17,7 @@ function getComputerChoice(){
     }
 }
 
-function game(){
+/*function game(){
     let playerRecount = 0;
     let computerRecount = 0;
     for(let i = 0; i < 5; i++){
@@ -32,9 +36,11 @@ function game(){
     } else {
         console.log("You Lose! " + computerRecount + " vs " + playerRecount + "!")
     }
-}
+}*/
 
-function playRound(playerChoice, computerChoice){
+function playRound(e){
+    let playerChoice = this.classList[0];
+    let computerChoice = getComputerChoice();
     let matchResult;
     switch(playerChoice.toUpperCase()){
         case "ROCK": 
@@ -67,19 +73,37 @@ function playRound(playerChoice, computerChoice){
                                  break;
             }
             break;
-    }        
+    }
+    const resultText = document.querySelector(".result");
+    const counter = document.querySelector(".counter");        
      switch(matchResult){
-         case 0: console.log("You Lose! " + computerChoice + " beats " + playerChoice +"!");
+         case 0: resultText.textContent = `You Lose! ${computerChoice} beats ${playerChoice} !`;
+                 gamesCounter++
+                 compCounter++
                  break;
-         case 1: console.log("You Win! " + playerChoice + " beats " + computerChoice + "!");
+         case 1: resultText.textContent = `You Win! ${playerChoice} beats ${computerChoice} !`;
+                 gamesCounter++
+                 playerCounter++
                 break;  
-         case -1: console.log("It's a Tie! Try again!");
+         case -1: resultText.textContent = "It's a Tie! Try again!";
                   break;              
                     
+    }
+
+    counter.textContent = `Player Score => ${playerCounter} vs ${compCounter} <= Computer Score`
+
+    if (gamesCounter === 5){
+        if(playerCounter >= 3) resultText.textContent = `FIve Games Played! You win ${playerCounter} vs ${compCounter}`
+        else resultText.textContent = `Five Games Played! You Lose! ${playerCounter} vs ${compCounter}`;
+        gamesCounter = 0;
+        playerCounter= 0;
+        compCounter = 0;
     }
     return matchResult;
 }
 
+const buttons = document.querySelectorAll('button');
 
+buttons.forEach(button => button.addEventListener('click', playRound));
 
     
